3ec1ea18cf912aaa6ce28a4ba0591286f2bc2af5
Azition <azition93@gmail.com>
2017-06-08 22:04:42 +0300

    Added windows and buttons

--- dev/js/common/app.js
+++ dev/js/common/app.js
@@ -1,150 +1,126 @@
 (function(w){
 
-	Vue.component('jumbotron', {
-		template: '#jumbotron',
-		props: {
-			message: String,
-			width: Number,
-			height: Number,
-		},
-		data: function(){
-			var that = this;
-			return {
-				absoluteContainerStyle:{
-					position: 'absolute',
-					bottom: '0px',
-					margin: '0 70px',
-					left: '450px',
-				},
-				lineStyle: {
-					height: '300px',
-				},
-				flags:{
-					stopResizeBottomHeight: false,
-				}
-			}
-		},
+	Vue.component('main', {
+		template: '#main',
 
 		ready: function(){
 			var that = this;
-			that.handlerHeight(w.innerHeight || w.document.body.clientHeight);
-			that.handlerWidth(w.innerWidth || w.document.body.clientWidth);
-			
-			if (w.attachEvent){
-				w.attachEvent('onresize', function(){
-					console.log('attachEvent - resize');
-					that.width = w.innerWidth || w.document.body.clientWidth;
-					that.height = w.innerHeight || w.document.body.clientHeight;
-					that.handlerHeight(that.height);
-					that.handlerWidth(that.width);				
-				});
-			} else if (w.addEventListener){
-				w.addEventListener('resize', function(){
-					that.width = w.innerWidth || w.document.body.clientWidth;
-					that.height = w.innerHeight || w.document.body.clientHeight;
-					that.handlerHeight(that.height);
-					that.handlerWidth(that.width);
-				}, true);
-			}
 		},
 
 		methods:{
-			handlerHeight: function(val){
-				if (!this.flags['stopResizeBottomHeight'])
-					this.absoluteContainerStyle['bottom'] = val*0.2+'px';
-			},
-			handlerWidth: function(val){
-				if (val > 1200){
-					this.absoluteContainerStyle['margin'] = '0 70px';
-					this.absoluteContainerStyle['left'] = '450px';
-					this.flags['stopResizeBottomHeight'] = false;
-					this.lineStyle['height'] = '300px';
-				} else if (val < 1200 && val >= 980){
-					this.absoluteContainerStyle['margin'] = '0 30px';
-					this.absoluteContainerStyle['left'] = '400px';
-					this.flags['stopResizeBottomHeight'] = false;
-					this.lineStyle['height'] = '300px';
-				} else if (val < 980 && val >= 700){
-					this.absoluteContainerStyle['margin'] = '0';
-					this.absoluteContainerStyle['left'] = (val / 2 - 260) + 'px';
-					this.flags['stopResizeBottomHeight'] = true;
-					this.absoluteContainerStyle['bottom'] = '60px';
-					this.lineStyle['height'] = '300px';
-				} else if (val < 700 && val >= 530){
-					this.absoluteContainerStyle['margin'] = '0';
-					this.absoluteContainerStyle['left'] = (val / 2 - 260) + 'px';
-					this.flags['stopResizeBottomHeight'] = true;
-					this.absoluteContainerStyle['bottom'] = '60px';
-					this.lineStyle['height'] = '200px';
-				} else if (val < 530 && val >= 0){
-					this.absoluteContainerStyle['margin'] = '0';
-					this.absoluteContainerStyle['left'] = (val / 2 - 160) + 'px';
-					this.flags['stopResizeBottomHeight'] = true;
-					this.absoluteContainerStyle['bottom'] = '60px';
-					this.lineStyle['height'] = '200px';
-				}
+			onOpen: function(val){
+				this.$dispatch('open-window',val);
 			}
 		}
 	});
 
 	Vue.component('profile',{
 		template: '#profile',
+		props:{
+			class: String,
+		},
 		data: function(){
 			return {
 			};
+		},
+		methods:{
+			onClose: function(){
+				this.$dispatch('close-window');
+			}
 		}
 	});
 
 	Vue.component('abilities',{
 		template: '#abilities',
+		props:{
+			class: String,
+		},
 		data: function(){
 			return {
 			};
+		},
+		methods:{
+			onClose: function(){
+				this.$dispatch('close-window');
+			}
 		}
 	});
 
 	Vue.component('projects', {
 		template: '#projects',
+		props:{
+			class: String,
+		},
 		data: function(){
 			return {
 			};
+		},
+		methods:{
+			onClose: function(){
+				this.$dispatch('close-window');
+			}
 		}
 	});
 
 	Vue.component('contacts',{
 		template: '#contacts',
+		props:{
+			class: String,
+		},
 		data: function(){
 			return{
 			};
+		},
+		methods:{
+			onClose: function(){
+				this.$dispatch('close-window');
+			}
 		}
-	});
-
-	Vue.component('cusbutton', {
-		template: '#button',
-
-		props: {
-			icon: String,
-		},
-
-		data: function(){
-			return {
-				name: 'Name',
-			};
-		},
 	});
 
 	new Vue({
 		el: '#app',
 		data: {
 			message: 'Interactive Resume',
-			jumbotron: {
-				width: 0,
-				height: 0,
+			rootClass:{
+				'open-left': false,
+				'open-right': false,
+				'open-top': false,
+				'open-bottom': false,
 			}
 		},
 		ready: function(){
-			this.jumbotron.width = w.innerWidth || w.document.body.clientWidth;
-			this.jumbotron.height = w.innerHeight || w.document.body.clientHeight;
+		},
+		methods:{
+			closeWindow: function(){
+				var that = this;
+				Object.keys(this.rootClass).map(function(k,v){
+					that.rootClass[k] = false;
+				});
+			}
+		},
+		events: {
+			'open-window': function(msg){
+				console.log(msg);
+				this.closeWindow();
+				switch(msg){
+					case 'top':
+						this.rootClass['open-top'] = true;
+						break;
+					case 'left':
+						this.rootClass['open-left'] = true;
+						break;
+					case 'bottom':
+						this.rootClass['open-bottom'] = true;
+						break;
+					case 'right':
+						this.rootClass['open-right'] = true;
+						break;
+				}
+			},
+			'close-window': function(){
+				this.closeWindow();
+			},
 		}
 	});
 })(window);
